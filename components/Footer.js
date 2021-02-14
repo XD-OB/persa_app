eb9d1be354c9d890f8723e1199029fec29ef3576
XD-OB <OB-96@hotmail.com>
2021-02-14 21:28:58 +0100

    Init project

---
+++ components/Footer.js
@@ -0,0 +1,7 @@
+const Footer = () => {
+    return (
+        <footer>Copyright 2021 Persa IO</footer>
+    );
+}
+ 
+export default Footer;
---
+++ components/Navbar.js
@@ -0,0 +1,11 @@
+const Navbar = () => {
+    return (
+        <nav>
+            <div className="logo">
+                <h1>Persa</h1>
+            </div>
+        </nav>
+    );
+}
+ 
+export default Navbar;
